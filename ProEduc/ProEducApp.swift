7b352982aad5db1fbfc1eb02d6725ecb7c6bf1f5
LaWass1864 <amwass4@hotmail.com>
2023-10-26 11:10:19 +0200

    Initial Commit

---
+++ ProEduc/ProEducApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProEducApp.swift
+//  ProEduc
+//
+//  Created by Apprenant 76 on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProEducApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
