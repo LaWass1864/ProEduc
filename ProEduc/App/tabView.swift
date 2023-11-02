fde691cea766c0ac111e44e004b9df234d9a5bb5
LaWass1864 <amwass4@hotmail.com>
2023-11-02 17:11:40 +0100

    dernier commit de ce jour pour Zac

--- ProEduc/App/tabView.swift
+++ ProEduc/App/tabView.swift
@@ -22,7 +22,7 @@ struct tabView: View {
                                           carteEntree: [DataBaseImage(id: "att9cM7UqgqFVTmwx", width: 754, height: 400, url: "https://v5.airtableusercontent.com/v2/22/22/1698948000000/u50pyiT6vBUp3Pp4Ve0Z6g/oZUkzrwQ0kfVObKhrAS3OTc6I2H75ZNnpd1nYfy6FijyBDSTepN6URf-JwH2i9yd-VWH79j74nO2scBaRlOCPIS5ldpsCEn8TJpnnKayW8vtucwmyPq20ihydFdricg0tdTmq1Ru2Lk-okFwsMcLVA/psp-tgrw98TnLqlLSEVw9l45wY7tO97DeBUMcqknaS8", filename: "carte-06.jpg", size: 2800, type: "image/jpeg", thumbnails: Thumbnails(small: .init(url: "", width: 0, height: 0), large: .init(url: "", width: 0, height: 0), full: .init(url: "", width: 1000, height: 1000)))],
                                           carteSelf: [DataBaseImage(id: "att9cM7UqgqFVTmwx", width: 754, height: 400, url: "https://v5.airtableusercontent.com/v2/22/22/1698948000000/u50pyiT6vBUp3Pp4Ve0Z6g/oZUkzrwQ0kfVObKhrAS3OTc6I2H75ZNnpd1nYfy6FijyBDSTepN6URf-JwH2i9yd-VWH79j74nO2scBaRlOCPIS5ldpsCEn8TJpnnKayW8vtucwmyPq20ihydFdricg0tdTmq1Ru2Lk-okFwsMcLVA/psp-tgrw98TnLqlLSEVw9l45wY7tO97DeBUMcqknaS8", filename: "image/jpeg", size: 2800, type: "image/jpeg", thumbnails: Thumbnails(small: .init(url: "", width: 0, height: 0), large: .init(url: "", width: 0, height: 0), full: .init(url: "", width: 1000, height: 1000)))],
                                           idFromEleve: ["recpA7eXWaWZoYT01"],
-                                          eleve: ["String"])
+                                          eleve: ["Hugo"])
 
                                           
     var body: some View {
