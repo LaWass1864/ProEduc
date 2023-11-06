82ca76ad10d858aa7edbe226b5a6b2aabcc922dd
lida8182 <lida8182@gmail.com>
2023-11-06 16:10:46 +0100

    select enfant

---
+++ ProEduc/Views/Eleve/ChangePassword.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePassword.swift
+//  ProEduc
+//
+//  Created by apprenant45 on 06/11/2023.
+//
+
+import SwiftUI
+
+struct ChangePassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePassword()
+}
